f2620b1d705b8988af2a7c569773cd6b1ff7a779
Nikhil Kamble <116864452+nikhilkamble02@users.noreply.github.com>
2023-09-20 23:01:46 +0530

    Add files via upload

---
+++ String/Remove_whitespace_from_string.js
@@ -0,0 +1,12 @@
+
+  
+  const removeWhitespaces = (string) => {
+    let result = '';
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] !== ' ' && string[i] !== '\n' && string[i] !== '\t') {
+        result += string[i];
+      }
+    }
+    return result;
+  }
+  removeWhitespaces(" Hello  , ")

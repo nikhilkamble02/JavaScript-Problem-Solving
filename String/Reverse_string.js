4ee02fddb2a6584ab830fcbdf74690111a85182c
Nikhil Kamble <116864452+nikhilkamble02@users.noreply.github.com>
2023-09-27 22:36:02 +0530

    Update Reverse_string.js

--- String/Reverse_string.js
+++ String/Reverse_string.js
@@ -1,9 +1,19 @@
+// first way
 const reverseAString = (str) => {
   let strArr = str.split(''); //convert string to array
   let reversedArr = strArr.reverse(); // reverse the array
   let reversedString = reversedArr.join(""); // convert revered array to string
   return reversedString
+} 
 
-}
+
+// 2nd way
+// const reverseAString = (str) => {
+//   let result = '';
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     result += str[i];
+//   }
+//   return result;
+// };
 
 console.log(reverseAString("nikhil"))

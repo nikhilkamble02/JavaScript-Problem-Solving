596eb1dfd17cf96a227b7a8ff3f75e4c6ca8917f
Nikhil Kamble <116864452+nikhilkamble02@users.noreply.github.com>
2023-12-18 22:40:34 +0530

    Update Reverse_array.js

--- Array/Reverse_array.js
+++ Array/Reverse_array.js
@@ -8,5 +8,5 @@ const reverseArray = (array) => {
 
 }
 
-let array = [1,2,3,4];
-console.log(reverseArray(array)); //[4,3,2,1]
+let array = [1,2,3,4,5];
+console.log(reverseArray(array)); //[5,4,3,2,1]

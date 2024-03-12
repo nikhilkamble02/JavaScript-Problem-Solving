5eacfbb66629dc36602d90c45e7aa1520b499fff
Nikhil Kamble <116864452+nikhilkamble02@users.noreply.github.com>
2024-03-12 15:44:44 +0530

    Update Remove_whitespace_from_string.js
    Updated Solution

--- String/Remove_whitespace_from_string.js
+++ String/Remove_whitespace_from_string.js
@@ -1,13 +1,11 @@
-
-  
   const removeWhitespaces = (string) => {
-    let result = '';
+   let finalString = '';
     for (let i = 0; i < string.length; i++) {
       if (string[i] !== ' ' && string[i] !== '\n' && string[i] !== '\t') {
         result += string[i];
       }
     }
-    return result;
+   return finalString;
   }
   removeWhitespaces(" Hello  World ") 
 //The function will return the string HelloWorld
